03dc0fcf21e8e4e34754190c729389613cedf2cf
Heather Chloe Wallace <heatherchloeis@gmail.com>
2018-09-07 14:35:28 -0700

    Update layout and style

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module CanaryApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = [:en]
+    config.i18n.available_locales = [:en] # your list of available locales here
   end
 end
